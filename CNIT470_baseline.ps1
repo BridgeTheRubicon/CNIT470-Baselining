888ee9c2fef2e67777c6c96423640319bb5cd4bd
Andrew <iamlegion1307@gmail.com>
2020-10-30 10:50:42 -0400

    Added the main 2 files

---
+++ CNIT470_baseline.ps1
@@ -0,0 +1,83 @@
+<#
+.SYNOPSIS
+A baselining script for incident response on Windows machines. This is specifically for CNIT470.
+REQUIERMENTS: must be run as an NT authority admin
+.Notes
+Version:  1.0
+Author: Andrew Burmeister
+Creation Date:  10/16/20
+Purpose/Change: Initial script development
+#>
+# initialize functions
+function makeNewFile($fName)
+  {
+    Get-Child -Path C:\Temp\
+    New-Item -Path * -Name $fName+":"+(Get-Date -Format o)+.txt -ItemType File
+  }
+# create the file
+$out = makeNewFile((hostname)) | Out-File -FilePath .\$out
+#
+# get the baseline data
+$a = systeminfo
+$b = wmic.exe product get name, version
+$c = Get-Process
+$d = Get-CimInstance Win32_StartupCommand | Select-Object Name, command, Location, User |
+  Format-List
+$e = Get-Counter -Counter "\Processor(_Total)\% Processor Time" -SampleInterval 4 -MaxSamples 5
+$f = systeminfo | find "Available Physical Memory"
+$g = systeminfo | find "Virtual Memory: Available"
+$h = Get-Volume
+$i = Get-Counter -Counter (Get-Counter -ListSet ipv4).paths -SampleInterval 10 -MaxSamples 5
+Set-Location -Path C:\Windows\System32
+$j = ls | Get-FileHash -Algorithm SHA384 | Format-List
+Set-Location -Path C:\Windows\System32\drivers
+$k = ls | Get-FileHash -Algorithm SHA384 | Format-List
+Set-Location -Path C:\Windows\System32\drivers\etc
+$l = ls | Get-FileHash -Algorithm SHA384 | Format-List
+#
+#write to the File
+Write-Output "Basic system information:"
+Write-Output $a | Out-File -FilePath .\$out
+Write-Output "`nInstalled Programs:"
+Write-Output $b | Out-File -FilePath .\$out
+Write-Output "`nRunning Services:"
+Write-Output $c | Out-File -FilePath .\$out
+Write-Output "`nStartup Programs:"
+Write-Output $d | Out-File -FilePath .\$out
+Write-Output "`nProcessor Utilization:"
+Write-Output $e | Out-File -FilePath .\$out
+Write-Output "`nMemory Utilization:"
+Write-Output $f | Out-File -FilePath .\$out
+Write-Output $g | Out-File -FilePath .\$out
+Write-Output "`nStorage Utilization:"
+Write-Output $h | Out-File -FilePath .\$out
+Write-Output "`nNetwork Utilization:"
+Write-Output $i | Out-File -FilePath .\$out
+Write-Output "`nFile Integrity:"
+Write-Output $j | Out-File -FilePath .\$out
+Write-Output $k | Out-File -FilePath .\$out
+Write-Output $l | Out-File -FilePath .\$out
+#
+# create the FtpWebRequest and configure it
+$ftp = [System.Net.FtpWebRequest]::Create("ftp://10.51.32.80/home/blueteam/baseline/$out")
+$ftp = [System.Net.FtpWebRequest]$ftp
+$ftp.Method = [System.Net.WebRequestMethods+Ftp]::UploadFile
+$ftp.Credentials = new-object System.Net.NetworkCredential("blueteam","HandsNoSwiping!")
+$ftp.UseBinary = $true
+$ftp.UsePassive = $true
+# read in the file to upload as a byte array
+$content = [System.IO.File]::ReadAllBytes("$out")
+$ftp.ContentLength = $content.Length
+# get the request stream, and write the bytes into it
+try
+{
+  $rs = $ftp.GetRequestStream()
+  $rs.Write($content, 0, $content.Length)
+}
+catch [System.Net.WebException]
+{
+  "Unable to upload the file."
+}
+# be sure to clean up after ourselves
+$rs.Close()
+$rs.Dispose()
---
+++ CNIT470_baseline.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+#This is a baselining script for *NIX-like machines. This is specifically for CNIT470.
+#Version: 1.0
+#Author:  Andrew Burmeister
+#Creation Date: 10/16/2020
+#
+FILE = uname+date+".txt"
+uname -a > $FILE
+echo "" | tee -a $FILE
+rpm -qa | less and dpkg -l >> $FILE
+echo "" | tee -a $FILE
+ps -aux >> $FILE
+echo "" | tee -a $FILE
+crontab -u root -l >> $FILE
+echo "" | tee -a $FILE
+top >> $FILE
+echo "" | tee -a $FILE
+top >> $FILE
+echo "" | tee -a $FILE
+top >> $FILE
+echo "" | tee -a $FILE
+ifstat >> $FILE
+echo "" | tee -a $FILE
+sha1sum  /bin >> $FILE
+sha1sum  /sbin >> $FILE
+sha1sum  /dev >> $FILE
+sha1sum  /etc >> $FILE
+#making the FTP connection
+HOST = "10.51.32.80"
+USER = "blueteam"
+PASS = "HandsNoSwiping!"
+#making the transfer (is weird becuase ftp is an interactive command)
+ftp -n $Host <<END
+quote USER $USER
+quote PASS $PASS
+binary
+put /home/blueteam/baseline/$FILE
+quit
+END
+#end of script
+exit 0
